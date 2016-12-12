0590c92f05a1083ac2d55239b49093df9e9a6798
mariel18-meet <marial-basous@hotmail.com>
2016-12-12 19:39:52 +0200

    big mistake

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -2,12 +2,12 @@
 from UserAccount import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAccount(  ,  ,  )
+my_user=UserAccount( "mariel18-meet" , "meetyear18" ,"I know michella's secret"  )
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
-
 #Use the wrong password as input here
-my_user.
+my_user.print_secret("meety1")
 
 #Use the right password here
-my_user.
+my_user.print_secret("meetyear18")
+                                         
